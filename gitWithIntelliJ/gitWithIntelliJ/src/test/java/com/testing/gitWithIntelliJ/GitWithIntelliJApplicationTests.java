a51a0c522cdb3b74eb65e7280fdda92eec78da8c
Anshuman Chinara <61976041+AnshumanChinara@users.noreply.github.com>
2024-07-07 17:01:59 +0530

    0001-us-springboot

---
+++ gitWithIntelliJ/gitWithIntelliJ/src/test/java/com/testing/gitWithIntelliJ/GitWithIntelliJApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testing.gitWithIntelliJ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitWithIntelliJApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
